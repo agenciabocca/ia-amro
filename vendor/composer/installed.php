b95824ac67703d7fbfebcb4eb70144ed0ed8e078
Bocca AMRO <douglas@agenciabocca.com.br>
2026-05-20 12:22:48 -0300

    Downgrade min PHP 8.2 -> 8.1 (alt-php82 nao tem PDO no Selector deste user)

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'amro/ia-suporte',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '943449191aa541e603e341d0309d2ae460f7a95e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'amro/ia-suporte' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '943449191aa541e603e341d0309d2ae460f7a95e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
